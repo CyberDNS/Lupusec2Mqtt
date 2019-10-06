a040c0a2b91c08567f40b204831ab1243af6fc61
David Ney <david.ney@live.com>
2019-10-06 15:05:45 +0200

    Hello World working

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,8 +13,13 @@ namespace Lupusec2Mqtt
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -36,5 +42,7 @@ namespace Lupusec2Mqtt
                 });
             });
         }
+
+
     }
 }
