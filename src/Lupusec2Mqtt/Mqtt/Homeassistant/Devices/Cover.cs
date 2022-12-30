239cd3980c3afce74a071fe60e44deb4894e313d
David Ney <david.ney@live.com>
2022-12-30 15:28:13 +0100

    fix: null reference exception

--- src/Lupusec2Mqtt/Mqtt/Homeassistant/Devices/Cover.cs
+++ src/Lupusec2Mqtt/Mqtt/Homeassistant/Devices/Cover.cs
@@ -42,7 +42,7 @@ namespace Lupusec2Mqtt.Mqtt.Homeassistant.Devices
         private Task<string> GetPosition(ILogger logger, ILupusecService lupusecService)
         {
             var shutter = lupusecService.SensorList.Sensors.Single(s => s.SensorId == GetStaticValue<string>("unique_id"));
-            var result = shutter.Level.ToString();
+            var result = shutter.Level;
 
             return Task.FromResult(result);
         }
@@ -67,73 +67,3 @@ namespace Lupusec2Mqtt.Mqtt.Homeassistant.Devices
         }
     }
 }
-
-
-
-
-//public class Cover : Device, IDevice, IStateProvider, IPositionProvider
-//{
-//    protected readonly Sensor _shutter;
-
-//    [JsonProperty("state_topic")]
-//    public string StateTopic => EscapeTopic($"homeassistant/{_component}/lupusec/{UniqueId}/state");
-
-//    [JsonProperty("position_topic")]
-//    public string PositionTopic => EscapeTopic($"homeassistant/{_component}/lupusec/{UniqueId}/position");
-
-//    [JsonProperty("position_open")]
-//    public int PositionOpen => 100;
-
-//    [JsonProperty("position_closed")]
-//    public int PositionClosed => 0;
-
-//    [JsonProperty("set_position_topic")]
-//    public string PositionCommandTopic => EscapeTopic($"homeassistant/{_component}/lupusec/{UniqueId}/set-pos");
-
-//    [JsonProperty("command_topic")]
-//    public string StateCommandTopic => EscapeTopic($"homeassistant/{_component}/lupusec/{UniqueId}/set");
-
-//    protected override string _component => "cover";
-
-//    [JsonIgnore]
-//    public string State => GetState();
-
-//    [JsonIgnore]
-//    public int Position => GetPosition();
-
-//    public Cover(IConfiguration configuration, Sensor shutter)
-//        : base(configuration)
-//    {
-//        _shutter = shutter;
-
-//        UniqueId = shutter.SensorId;
-//        Name = GetValue(nameof(Name), shutter.Name);
-
-//        Commands = new Command[]
-//        {
-//                new Command(StateCommandTopic, ExecuteCommand),
-//                new Command(PositionCommandTopic, ExecuteCommand),
-//        };
-
-//    }
-
-//    private string GetState()
-//    {
-//        if (_shutter.Level <= 0) { return "closed"; }
-//        else { return "open"; }
-//    }
-
-//    private int GetPosition()
-//    {
-//        return _shutter.Level;
-//    }
-
-//    public void ExecuteCommand(string command, ILupusecService lupusecService)
-//    {
-//        if (command.Equals("OPEN")) { command = "on"; }
-//        else if (command.Equals("CLOSE")) { command = "off"; }
-//        else if (command.Equals("STOP")) { command = "stop"; }
-
-//        lupusecService.SetCoverPosition(_shutter.Area, _shutter.Zone, command);
-//    }
-//}
